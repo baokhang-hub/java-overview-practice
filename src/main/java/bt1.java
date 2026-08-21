91844b898c70931a31e3e854d857d40704a0ec15
baokhang-hub <mtbaokhang@gmail.com>
2026-08-21 13:31:25 +0700

    bai tap 1

---
+++ src/main/java/bt1.java
@@ -0,0 +1,12 @@
+public class bt1 {
+    public static void main(String[] args) {
+        double height = 5;
+        double width = 7;
+
+        double S = width * height;
+        double P = (width + height) * 2;
+
+        System.out.println("Dien tich hcn la: " + S);
+        System.out.println("Chu vi hcn la: " + P);
+    }
+}
